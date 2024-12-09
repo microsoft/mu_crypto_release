72aa3ec3aa16cff9ac4ef712835b7fa338085cb1
Doug Flick <dougflick@microsoft.com>
2024-12-09 15:16:24 -0800

    add debug support and random

--- OpensslPkg/Library/Include/SharedCrtLibSupport.h
+++ OpensslPkg/Library/Include/SharedCrtLibSupport.h
@@ -34,9 +34,16 @@ typedef EFI_STATUS (EFIAPI *GET_TIME)(
   OUT  EFI_TIME_CAPABILITIES  *Capabilities OPTIONAL
   );
 
-// AsciiStrCpyS
-// AsciiStrCmp
+/**
+  Copies a string to a destination buffer.
 
+  @param[out]  Destination  Pointer to the destination buffer.
+  @param[in]   DestMax      Maximum size of the destination buffer.
+  @param[in]   Source       Pointer to the source string.
+
+  @retval RETURN_SUCCESS           The string was copied successfully.
+  @retval RETURN_BUFFER_TOO_SMALL  The destination buffer is too small.
+**/
 typedef
 RETURN_STATUS
 (EFIAPI *ASCII_STR_CPY_S)(
@@ -44,6 +51,151 @@ RETURN_STATUS
     IN  UINTN        DestMax,
     IN  CONST CHAR8  *Source
     );
+
+/**
+  Returns the size of a Null-terminated ASCII string in bytes, including the Null terminator.
+
+  @param[in]  String  Pointer to the Null-terminated ASCII string.
+
+  @return The size of the Null-terminated ASCII string in bytes, including the Null terminator.
+**/
+typedef UINTN (EFIAPI *ASCII_STR_SIZE)(
+  IN      CONST CHAR8  *String
+  );
+
+/**
+  Compares two Null-terminated ASCII strings.
+
+  @param[in]  FirstString   Pointer to the first Null-terminated ASCII string.
+  @param[in]  SecondString  Pointer to the second Null-terminated ASCII string.
+
+  @retval 0   The strings are identical.
+  @retval <0  The first string is less than the second string.
+  @retval >0  The first string is greater than the second string.
+**/
+typedef INTN (EFIAPI *ASCII_STR_CMP)(
+  IN      CONST CHAR8  *FirstString,
+  IN      CONST CHAR8  *SecondString
+  );
+/**
+  Returns the length of a Null-terminated ASCII string.
+
+  @param[in]  String  Pointer to the Null-terminated ASCII string.
+  @param[in]  MaxSize Maximum number of ASCII characters to examine.
+
+  @return The length of the Null-terminated ASCII string.
+**/
+typedef UINTN (EFIAPI *ASCII_STRN_LEN_S)(
+  IN CONST CHAR8  *String,
+  IN UINTN        MaxSize
+  );
+
+/**
+  Compares two Null-terminated ASCII strings with a specified length.
+
+  @param[in]  FirstString   Pointer to the first Null-terminated ASCII string.
+  @param[in]  SecondString  Pointer to the second Null-terminated ASCII string.
+  @param[in]  Length        Maximum number of ASCII characters to compare.
+
+  @retval 0   The strings are identical.
+  @retval <0  The first string is less than the second string.
+  @retval >0  The first string is greater than the second string.
+**/
+typedef INTN (EFIAPI *ASCII_STRN_CMP)(
+  IN      CONST CHAR8  *FirstString,
+  IN      CONST CHAR8  *SecondString,
+  IN      UINTN        Length
+  );
+
+/**
+  Converts a Null-terminated ASCII string to an unsigned integer.
+
+  @param[in]  String  Pointer to the Null-terminated ASCII string.
+
+  @return The unsigned integer value of the string.
+**/
+typedef UINTN (EFIAPI *ASCII_STR_DECIMAL_TO_UINTN)(
+  IN      CONST CHAR8  *String
+  );
+
+/**
+  Copies a specified length of a string to a destination buffer.
+
+  @param[out]  Destination  Pointer to the destination buffer.
+  @param[in]   DestMax      Maximum size of the destination buffer.
+  @param[in]   Source       Pointer to the source string.
+  @param[in]   Length       Number of characters to copy.
+
+  @retval RETURN_SUCCESS           The string was copied successfully.
+  @retval RETURN_BUFFER_TOO_SMALL  The destination buffer is too small.
+**/
+typedef RETURN_STATUS (EFIAPI *ASCII_STRN_CPY_S)(
+  OUT CHAR8        *Destination,
+  IN  UINTN        DestMax,
+  IN  CONST CHAR8  *Source,
+  IN  UINTN        Length
+  );
+
+/**
+  Compares two Null-terminated ASCII strings in a case-insensitive manner.
+
+  @param[in]  FirstString   Pointer to the first Null-terminated ASCII string.
+  @param[in]  SecondString  Pointer to the second Null-terminated ASCII string.
+
+  @retval 0   The strings are identical.
+  @retval <0  The first string is less than the second string.
+  @retval >0  The first string is greater than the second string.
+**/
+typedef INTN (EFIAPI *ASCII_STRI_CMP)(
+  IN      CONST CHAR8  *FirstString,
+  IN      CONST CHAR8  *SecondString
+  );
+
+/**
+  Concatenates a source string to the end of a destination string.
+
+  @param[in, out]  Destination  Pointer to the destination buffer.
+  @param[in]       DestMax      Maximum size of the destination buffer.
+  @param[in]       Source       Pointer to the source string.
+
+  @retval RETURN_SUCCESS           The string was concatenated successfully.
+  @retval RETURN_BUFFER_TOO_SMALL  The destination buffer is too small.
+**/
+typedef RETURN_STATUS (EFIAPI *ASCII_STR_CAT_S)(
+  IN OUT CHAR8        *Destination,
+  IN     UINTN        DestMax,
+  IN     CONST CHAR8  *Source
+  );
+
+/**
+  Produces a Null-terminated ASCII string in an output buffer based on a Null-terminated
+  ASCII format string and variable argument list.
+
+  @param[out]  StartOfBuffer  Pointer to the output buffer.
+  @param[in]   BufferSize     Size of the output buffer in bytes.
+  @param[in]   FormatString   Pointer to the Null-terminated ASCII format string.
+  @param[in]   ...            Variable argument list.
+
+  @return The number of ASCII characters printed to the output buffer, not including
+          the Null-terminator.
+**/
+typedef UINTN (EFIAPI *ASCII_S_PRINT)(
+  OUT CHAR8        *StartOfBuffer,
+  IN  UINTN        BufferSize,
+  IN  CONST CHAR8  *FormatString,
+  ...
+  );
+
+/**
+  Returns the length of a Null-terminated ASCII string.
+
+  @param[in]  String  Pointer to the Null-terminated ASCII string.
+
+  @return The length of the Null-terminated ASCII string.
+**/
+typedef UINTN (EFIAPI *ASCII_STR_LEN)(
+  IN      CONST CHAR8  *String
+  );
 
 typedef
 BOOLEAN
@@ -60,6 +212,17 @@ typedef struct  _SHARED_DEPENDENCIES {
   GET_TIME                GetTime;
   DEBUG_PRINT             DebugPrint;
   GET_RANDOM_NUMBER_64    GetRandomNumber64;
+  ASCII_STR_CPY_S         AsciiStrCpyS;
+  ASCII_STR_SIZE          AsciiStrSize;
+  ASCII_STR_CMP           AsciiStrCmp;
+  ASCII_STRN_LEN_S        AsciiStrnLenS;
+  ASCII_STRN_CMP          AsciiStrnCmp;
+  ASCII_STR_DECIMAL_TO_UINTN  AsciiStrDecimalToUintn;
+  ASCII_STRN_CPY_S        AsciiStrnCpyS;
+  ASCII_STRI_CMP          AsciiStriCmp;
+  ASCII_STR_CAT_S         AsciiStrCatS;
+  ASCII_S_PRINT           AsciiSPrint;
+  ASCII_STR_LEN           AsciiStrLen;
 } SHARED_DEPENDENCIES;
 
 SHARED_DEPENDENCIES  *gSharedDepends;
@@ -70,6 +233,18 @@ SHARED_DEPENDENCIES  *gSharedDepends;
 ///////////////////////////////////////////////////////////////////////////////
 
 #define ASSERT  gSharedDepends->ASSERT;
+
+// TODO might make more sense to move this to a separate file
+#define DEBUG_ERROR  0x80000000
+#define DEBUG_WARN   0x40000000
+#define DEBUG_INFO   0x20000000
+#define DEBUG_VERBOSE 0x10000000
+#define DEBUG(Args) \
+  do { \
+    if (gSharedDepends != NULL && gSharedDepends->DebugPrint != NULL) { \
+      gSharedDepends->DebugPrint Args; \
+    } \
+  } while (0)
 
 VOID *
 EFIAPI
@@ -235,5 +410,20 @@ AsciiSPrint (
   IN  CONST CHAR8  *FormatString,
   ...
   );
+
+UINT16
+EFIAPI
+WriteUnaligned16 (
+  OUT UINT16  *Buffer,
+  IN  UINT16  Value
+  );
+
+UINT32
+EFIAPI
+WriteUnaligned32 (
+  OUT UINT32  *Buffer,
+  IN  UINT32  Value
+  );
+
 
 #endif // SHARED_CRT_LIB_H
