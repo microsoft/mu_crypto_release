820e2231a94ad1eb43d9379871d947a87e333b1e
DorLevi95 <dorlevi@microsoft.com>
2024-12-23 11:43:31 +0200

    devide flow to different compare functions for clearer view

--- .azuredevops/scripts/CompareCryptoReports.py
+++ .azuredevops/scripts/CompareCryptoReports.py
@@ -98,41 +98,19 @@ def parse_report(report_file, report_info, binary_sizes_per_arch, linked_openssl
                 flags_list = line.split("=")[1].strip().split()
                 openssl_conf_report[current_openssl_lib_file][define_openssl_flags] = flags_list
 
-def compare_reports(source_reports, target_reports):
-    # compare basic report information
-    print("Source Branch Report Info:\n")
-    print(f"Branch and Commit: {source_reports['info']['branch_commit']}")
-    print(f"Flavor: {source_reports['info']['flavor']}\n")
-    print(f"Build Target: {source_reports['info']['build_target']}\n")
-    print(f"Tool Chain: {source_reports['info']['tool_chain']}\n")
-    print(f"Build Time: {source_reports['info']['Build Time']}\n\n")
-
-    print("Target Branch Report Info:\n")
-    print(f"Branch and Commit: {target_reports['info']['branch_commit']}")
-    print(f"Flavor: {target_reports['info']['flavor']}\n")
-    print(f"Build Target: {target_reports['info']['build_target']}\n")
-    print(f"Tool Chain: {target_reports['info']['tool_chain']}\n")
-    print(f"Build Time: {target_reports['info']['Build Time']}\n\n")
-
-    if(source_reports['info']['flavor'] != target_reports['info']['flavor']):
-        log_warning("Reports are for different flavors!")
-    
-    if(source_reports['info']['build_target'] != target_reports['info']['build_target']):
-        log_warning("Reports are for different build targets!")
-    
-
-    # compare binaries sizes per architecture
-    for arch in source_reports['sizes_per_arch']:
-        log_section(f"Comparing binary sizes for Arch: {arch}:")
-        if arch not in target_reports['sizes_per_arch']:
+def compare_sizes(source_sizes, target_sizes):
+     # compare binaries sizes per architecture
+    for arch in source_sizes:
+        log_section(f"Comparing binary sizes for Arch {arch}:")
+        if arch not in target_sizes:
             log_warning(f"Architecture {arch} not found in target report!")
             continue
-        for binary in source_reports['sizes_per_arch'][arch]:
-            if binary not in target_reports['sizes_per_arch'][arch]:
+        for binary in source_sizes[arch]:
+            if binary not in target_sizes[arch]:
                 log_warning(f"A new binary has been added! - {binary} is not found in target report for architecture {arch}") # log warning - New Binary!
                 continue
-            source_binary_size = source_reports['sizes_per_arch'][arch][binary]
-            target_binary_size = target_reports['sizes_per_arch'][arch][binary]
+            source_binary_size = source_sizes[arch][binary]
+            target_binary_size = target_sizes[arch][binary]
 
             # get compressed and uncompressed sizes
             source_binary_size_uncompressed = float(source_binary_size['Uncompressed size'].split()[0])
@@ -159,56 +137,89 @@ def compare_reports(source_reports, target_reports):
                     f"Compressed size for {binary} in architecture {arch} has decreased by {size_diff_compressed}%!")  # log warning - Decrease in size!
         
         # check if a binary is missing in source report
-        for binary in target_reports['sizes_per_arch'][arch]:
-            if binary not in source_reports['sizes_per_arch'][arch]:
+        for binary in target_sizes[arch]:
+            if binary not in source_sizes[arch]:
                 log_warning(f"A binary has been removed! - {binary} is not found in source report for architecture {arch}") # log warning - Missing Binary!
 
-    # compare linked openssl lib for binary per architecture
-    # already reported new/missing binaries in the sizes comparison, so no need to report them again here
-    for arch in source_reports['linked_openssllib_per_arch']:
+def comapre_linked_openssllib(source_linked_openssllib, target_linked_openssllib):
+    for arch in source_linked_openssllib:
         log_section(f"Comparing linked OpensslLib for Arch: {arch}:")
 
-        for binary in source_reports['linked_openssllib_per_arch'][arch]:
-            source_linked_openssllib = source_reports['linked_openssllib_per_arch'][arch][binary]
-            target_linked_openssllib = target_reports['linked_openssllib_per_arch'][arch][binary]
+        for binary in source_linked_openssllib[arch]:
+            source_linked_openssllib_for_binary = source_linked_openssllib[arch][binary]
+            target_linked_openssllib_for_binary = target_linked_openssllib[arch][binary]
 
-            if source_linked_openssllib != target_linked_openssllib:
-                log_warning(f"Linked OpensslLib has been changed for {binary} in architecture {arch}! - New {source_linked_openssllib}, Old {target_linked_openssllib}")  # log warning - Change in linked OpensslLib*.inf!
+            if source_linked_openssllib_for_binary != target_linked_openssllib_for_binary:
+                log_warning(
+                    f"Linked OpensslLib has been changed for {binary} in architecture {arch}! - New {source_linked_openssllib_for_binary}, Old {target_linked_openssllib_for_binary}")  # log warning - Change in linked OpensslLib*.inf!
 
-    # compare openssl configuration flags
-    for file in source_reports['openssl_libs_flags']:
-        log_section(f"Comparing OpensslLib configuration flags for file: {file}:")
-        if file not in target_reports['openssl_libs_flags']:
+def compare_openssl_flags(source_flags, target_flags):
+    for file in source_flags:
+        log_section(f"Comparing OpensslLib configuration flags for file {file}:")
+        if file not in target_flags:
             log_warning(f"A new OpensslLib*.inf has been added! - {file} is not found in target report") # log warning - New OpensslLib*.inf file!
             continue
         
         # compare "OPENSSL_FLAGS" 
-        source_flags = source_reports['openssl_libs_flags'][file]["OPENSSL_FLAGS"]
-        target_flags = target_reports['openssl_libs_flags'][file]["OPENSSL_FLAGS"]
+        source_flags_for_file = source_flags[file]["OPENSSL_FLAGS"]
+        target_flags_for_file = target_flags[file]["OPENSSL_FLAGS"]
 
-        for flag in source_flags:
-            if flag not in target_flags:
+        for flag in source_flags_for_file:
+            if flag not in target_flags_for_file:
                 log_warning(f"A new flag has been added in {file}! - In OPENSSL_FLAGS section, flag {flag} is not found in target report") # log warning - New flag in OpensslLib*.inf file!
 
-        for flag in target_flags:
-            if flag not in source_flags:
+        for flag in target_flags_for_file:
+            if flag not in source_flags_for_file:
                 log_warning(f"A flag has been removed in {file}! - In OPENSSL_FLAGS section, flag {flag} is not found in source report") # log warning - Missing flag in OpensslLib*.inf file!
 
         # compare "OPENSSL_FLAGS_CONFIG"
-        source_flags = source_reports['openssl_libs_flags'][file]["OPENSSL_FLAGS_CONFIG"]
-        target_flags = target_reports['openssl_libs_flags'][file]["OPENSSL_FLAGS_CONFIG"]
+        source_configflags_for_file = source_flags[file]["OPENSSL_FLAGS_CONFIG"]
+        target_configflags_for_file = target_flags[file]["OPENSSL_FLAGS_CONFIG"]
 
-        for flag in source_flags:
-            if flag not in target_flags:
+        for flag in source_configflags_for_file:
+            if flag not in target_configflags_for_file:
                 log_warning(f"A new flag has been added in {file}! - In OPENSSL_FLAGS_CONFIG section, flag {flag} is not found in target report") # log warning - New flag in OpensslLib*.inf file!
 
-        for flag in target_flags:
-            if flag not in source_flags:
+        for flag in target_configflags_for_file:
+            if flag not in source_configflags_for_file:
                 log_warning(f"A flag has been removed in {file}! - In OPENSSL_FLAGS_CONFIG section, flag {flag} is not found in source report") # log warning - Missing flag in OpensslLib*.inf file!
     
-    for file in target_reports['openssl_libs_flags']:
-        if file not in source_reports['openssl_libs_flags']:
+    for file in target_flags:
+        if file not in source_flags:
             log_warning(f"An OpensslLib*.inf has been removed! - {file} is not found in source report") # log warning - Missing OpensslLib*.inf file!
+
+def compare_reports(source_reports, target_reports):
+    # print and compare basic report information
+    print("Source Branch Report Info:\n")
+    print(f"Branch and Commit: {source_reports['info']['branch_commit']}")
+    print(f"Flavor: {source_reports['info']['flavor']}\n")
+    print(f"Build Target: {source_reports['info']['build_target']}\n")
+    print(f"Tool Chain: {source_reports['info']['tool_chain']}\n")
+    print(f"Build Time: {source_reports['info']['Build Time']}\n\n")
+
+    print("Target Branch Report Info:\n")
+    print(f"Branch and Commit: {target_reports['info']['branch_commit']}")
+    print(f"Flavor: {target_reports['info']['flavor']}\n")
+    print(f"Build Target: {target_reports['info']['build_target']}\n")
+    print(f"Tool Chain: {target_reports['info']['tool_chain']}\n")
+    print(f"Build Time: {target_reports['info']['Build Time']}\n\n")
+
+    if(source_reports['info']['flavor'] != target_reports['info']['flavor']):
+        log_warning("Reports are for different flavors!")
+    
+    if(source_reports['info']['build_target'] != target_reports['info']['build_target']):
+        log_warning("Reports are for different build targets!")
+    
+
+    # compare binary sizes per architecture
+    compare_sizes(source_reports['sizes_per_arch'], target_reports['sizes_per_arch'])
+
+    # compare linked openssl lib for binary per architecture
+    # already reported new/missing binaries in the sizes comparison, so no need to report them again here
+    comapre_linked_openssllib(source_reports['linked_openssllib_per_arch'], target_reports['linked_openssllib_per_arch'])
+
+    # compare openssl configuration flags
+    compare_openssl_flags(source_reports['openssl_libs_flags'], target_reports['openssl_libs_flags'])
 
     log_section("Comparison completed successfully!")
 
@@ -225,9 +236,10 @@ if __name__ == '__main__':
     log_section(f"Parsing target report file: {target_report_file}")
     parse_report(target_report_file, target_report_info, target_binary_sizes_per_arch, target_linked_openssllib_per_arch, target_openssl_conf_report)
 
-    # compare the reports
+    # wrap the parsed information in single dict
     source_reports = {"info": source_report_info, "sizes_per_arch": source_binary_sizes_per_arch, "linked_openssllib_per_arch": source_linked_openssllib_per_arch, "openssl_libs_flags": source_openssl_conf_report}
     target_reports = {"info": target_report_info, "sizes_per_arch": target_binary_sizes_per_arch, "linked_openssllib_per_arch": target_linked_openssllib_per_arch, "openssl_libs_flags": target_openssl_conf_report}
 
+    # compare the reports
     log_section("Comparing reports")
     compare_reports(source_reports, target_reports)
