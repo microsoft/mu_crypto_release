45a1839bbd24edcde1dad207ca185e71d454946c
Doug Flick <dougflick@microsoft.com>
2026-01-17 15:54:39 -0800

    OneCryptoPkg: Add GUID definitions
    Add GUID definitions for OneCrypto file identification:
    - OneCryptoBin.h: GUID for the OneCryptoBin executable file
    
    These GUIDs are used to identify and locate the OneCryptoBin binary
    file in the firmware volume, enabling the loader drivers to find and
    load the shared cryptographic binary.

---
+++ OneCryptoPkg/Include/Guid/OneCryptoFileGuid.h
@@ -0,0 +1,22 @@
+/** @file
+
+  This file contains the GUID for the phase agnostic shared one crypto binary
+
+  Copyright (C) Microsoft Corporation
+
+  SPDX-License-Identifier: BSD-2-Clause-Patent
+
+**/
+
+#ifndef ONE_CRYPTO_FILE_GUID_H_
+#define ONE_CRYPTO_FILE_GUID_H_
+
+//
+// FILE_GUID(76ABA88D-9D16-49A2-AA3A-DB6112FAC5CC) of OneCryptoMmBin.inf
+//
+#define ONE_CRYPTO_BINARY_GUID \
+  { \
+    0x76ABA88D, 0x9D16, 0x49A2, { 0xAA, 0x3A, 0xDB, 0x61, 0x12, 0xFA, 0xC5, 0xCC } \
+  }
+
+#endif // ONE_CRYPTO_FILE_GUID_H_
