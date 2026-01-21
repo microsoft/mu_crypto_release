14334912f5301562e8744e83b34645360011c088
Doug Flick <dougflick@microsoft.com>
2026-01-20 16:40:19 -0800

    OpensslPkg: Add RealTimeClockLib for time functions
    Update TimerWrapper.c to use RealTimeClockLib's LibGetTime() instead
    of directly calling gRT->GetTime(). This provides a more portable
    abstraction for retrieving system time.
    
    Changes:
    - Replace UefiRuntimeServicesTableLib.h include with RealTimeClockLib.h
    - Use LibGetTime() instead of gRT->GetTime()
    - Add RealTimeClockLib to library dependencies
    - Add MdeModulePkg.dec package dependency
    - Remove unused BaseMemoryLib dependency

--- OpensslPkg/Library/BaseCryptLib/SysCall/TimerWrapper.c
+++ OpensslPkg/Library/BaseCryptLib/SysCall/TimerWrapper.c
@@ -9,7 +9,7 @@ SPDX-License-Identifier: BSD-2-Clause-Patent
 
 #include <Uefi.h>
 #include <CrtLibSupport.h>
-#include <Library/UefiRuntimeServicesTableLib.h>
+#include <Library/RealTimeClockLib.h>
 
 //
 // -- Time Management Routines --
@@ -96,7 +96,7 @@ time (
   //
   // Get the current time and date information
   //
-  Status = gRT->GetTime (&Time, NULL);
+  Status = LibGetTime (&Time, NULL);
   if (EFI_ERROR (Status) || (Time.Year < 1970)) {
     return 0;
   }
