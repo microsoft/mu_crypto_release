980396266885530429f2a99595e287bdcebc2fd4
Doug Flick <dougflick@microsoft.com>
2026-01-28 16:29:25 -0800

    OneCryptoPkg: Setup GCC5 Symbol Export
    This commit adds the ability to export the CryptoEntry for GCC5.
    This requires changes to BaseTools in order to export the
    symbols from the ELF binary to the resulant PE32 EFI binary.
    
    Signed-off-by: Doug Flick <dougflick@microsoft.com>

--- OneCryptoPkg/OneCryptoBin/OneCryptoBin.c
+++ OneCryptoPkg/OneCryptoBin/OneCryptoBin.c
@@ -17,8 +17,11 @@
 
 #if defined (_MSC_EXTENSIONS)
   #define COMMON_EXPORT_API  __declspec(dllexport)
-#elif defined(__GNUC__) || defined(__clang__)
-  #define COMMON_EXPORT_API /*TODO Support GCC*/
+#elif defined(__GNUC__)
+  // noipa = no interprocedural analysis (GCC 8+), prevents LTO from inlining/merging
+  #define COMMON_EXPORT_API  __attribute__((used, visibility("default"), externally_visible, noinline, noipa))
+#elif defined(__clang__)
+  #define COMMON_EXPORT_API  __attribute__((used, visibility("default"), noinline, optnone))
 #else
   #define COMMON_EXPORT_API
 #endif
